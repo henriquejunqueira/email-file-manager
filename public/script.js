58c9eee319335d9dde0bc3596f3bec51b67f3a15
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-05-09 17:57:48 -0300

    Iniciada codificação inicial do script.js

---
+++ public/script.js
@@ -0,0 +1,5 @@
+const api = 'http://localhost:3000/publicacoes';
+
+const form = document.getElementById('recordForm');
+const lista = document.getElementById('recordsList');
+const searchInput = document.getElementById('searchCity');
